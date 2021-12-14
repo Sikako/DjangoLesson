94598408b51f1b0106b46fd4018960cd1bd87694
Sikako <71139205+Sikako@users.noreply.github.com>
2021-12-14 16:50:42 +0800

    新增bootstrap header

--- HelloWorld/HelloWorld/views.py
+++ HelloWorld/HelloWorld/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 
 # def Hello(request):
   # return HttpResponse("Hello World")
+
+# index被呼叫就回傳templates中的index.html並使用context去填充變量
 def index(request):
   context = {}
   context["name"] = "Sikako"
